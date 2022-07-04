4b001f61e797f2cc719a0ac7b3b3a55d2695decd
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2022-07-03 23:15:40 -0300

    add cloud & floor

--- js/script.js
+++ js/script.js
@@ -1,47 +1,43 @@
-let marioPosition;
 const mario = document.querySelector(".super-mario");
 const pipe = document.querySelector(".pipe-game");
 const fireball = document.querySelector(".fire-ball");
-const clouds = document.querySelector(".cloud-game");
+const cloud1 = document.querySelector(".cloud-game1");
+const cloud2 = document.querySelector(".cloud-game2");
 const floor = document.querySelector(".floor");
 const jump_snd = new Audio("../sounds/jump.wav");
 const fire_snd = new Audio("../sounds/fire.wav");
 const rotate_snd = new Audio("../sounds/giro.wav");
 const game_over_snd = new Audio("../sounds/game-over.wav");
 const track_snd = new Audio("../sounds/medley.mp3");
-/* const soundtrack = document.getElementById('audio'); */
-/* console.log(soundtrack); */
+let marioPosition;
 let game_on = true;
-/* const jump = function() {
-    mario.classList.add("jump-mario");
-} */
-/* autosound(); */
-/* track_snd.autoplay = true;
-track_snd.play();
-track_snd.load(); */
+let jump_on = false;
+let rotate_on = false;
+let fire_on = false;
 
 
-/* Air function, mesma função acima */
+/* Air function */
 const jump = () => {
     mario.classList.add("jump-mario");
-    //fireball.style.display = 'none';
     jump_snd.play();
+    jump_on = true;
 
     setTimeout(() => {
         mario.classList.remove("jump-mario");
-        //fireball.style.display = 'block';
+        jump_on = false;
+
     }, 500);
 }
 
 const rotate = () => {
     mario.classList.add("rotate-mario");
-    
-    //fireball.style.display = 'none';
     rotate_snd.play();
+    rotate_on = true;
 
     setTimeout(() => {
         mario.classList.remove("rotate-mario");
-        //fireball.style.display = 'block';
+        rotate_on = false;
+
     }, 500);
 }
 
@@ -50,16 +46,19 @@ const fire = () => {
     fireball.style.bottom = `${marioPosition + 40}px`;
     fireball.style.display = 'block';
     fire_snd.play();
+    fire_on = true;
 
     setTimeout(() => {
         fireball.classList.remove("shot-fireball");
         fireball.style.display = 'none';
+        fire_on = false;
     }, 1500);
 
 }
 
 const loopGame = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
+
     marioPosition = +window
         .getComputedStyle(mario)
         .bottom.replace("px", "");
@@ -72,22 +71,14 @@ const loopGame = setInterval(() => {
         marioPosition < distPipe) {
 
         mario.src = "./Images/mario-game-over.png";
-        mario.style.width = "9%";
+        mario.style.width = "7%";
         mario.style.marginLeft = "45%";
         mario.style.marginBottom = "25%";
 
         mario.style.bottom = `${marioPosition}px`;
         pipe.style.left = `${pipePosition}px`;
 
-        pipe.style.animation = "none";
-        mario.style.animation = "none";
-        clouds.style.animation = "none";
-        floor.style.animation = "none";
-
-        track_snd.pause();
-        track_snd.currentTime = 0;
-        game_over_snd.play();
-        game_on = false;
+        stopGame();
 
         clearInterval(loopGame);
     }
@@ -97,25 +88,39 @@ const loopGame = setInterval(() => {
 document.addEventListener("keydown", mapKeys)
 
 function mapKeys(event) {
-    if (event.keyCode === 32 && game_on) {
-        /* console.log("Espaço"); */
-        jump();
+    if (game_on) {
         track_snd.play();
-    }
-    if (event.keyCode === 13 && game_on) {
-        rotate();
-        track_snd.play();
-    }
-    if (event.keyCode === 16 && game_on) {
-        /* console.log("Ctrl"); */
-        fire();
+        if (!jump_on && !rotate_on) {
+            if (event.keyCode === 32) {
+                jump();
+            }
+            if (event.keyCode === 13) {
+                rotate();
+            }
+        }
+        if (event.keyCode === 18 && !fire_on) {
+            fire();
+        }
     }
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-/* 
-function autosound() {
-    track_snd.play();
-} */
+
+function stopGame() {
+    pipe.style.animation = "none";
+    mario.style.animation = "none";
+    cloud1.style.animation = "none";
+    cloud2.style.animation = "none";
+    floor.style.animation = "none";
+    fireball.style.animation = "none";
+    cloud1.style.right = "0px";
+    cloud2.style.left = "-100px";
+
+
+    track_snd.pause();
+    track_snd.currentTime = 0;
+    game_over_snd.play();
+    game_on = false;
+}
